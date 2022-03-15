e57b7304e877118c49c90c5b4e6a06fe2da4b949
Georgii Vlasov <honest.niceman@gmail.com>
2022-03-15 20:22:19 +0400

    Entity and Repos

---
+++ src/main/java/jpa/buddy/transactionaltests/entities/NewEntity.java
@@ -0,0 +1,34 @@
+package jpa.buddy.transactionaltests.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.UUID;
+
+@Entity
+@Table(name = "new_entity")
+public class NewEntity {
+    @Id
+    @Column(name = "id", nullable = false)
+    private UUID id;
+
+    @Column(name = "name")
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+}
