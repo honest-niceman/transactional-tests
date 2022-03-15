2dded71cd6f6bac960a61bf7fcda5496ffbcedc5
Georgii Vlasov <honest.niceman@gmail.com>
2022-03-15 20:21:41 +0400

    Init commit

---
+++ src/main/java/jpa/buddy/transactionaltests/repositories/RepositoryTransactionalReadOnlyTrue.java
@@ -0,0 +1,21 @@
+package jpa.buddy.transactionaltests.repositories;
+
+import jpa.buddy.transactionaltests.entities.NewEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Transactional(readOnly = true)
+public interface RepositoryTransactionalReadOnlyTrue extends JpaRepository<NewEntity, UUID> {
+    @Query("delete from NewEntity n where n.name = ?1")
+    @Transactional
+    @Modifying
+    void deleteTransactionalAndModifying(String name);
+
+    @Query("delete from NewEntity n where n.name = ?1")
+    @Modifying
+    void deleteModifying(String name);
+}
